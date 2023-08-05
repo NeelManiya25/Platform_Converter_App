17572eb6ae33764f8e391eca6213d119f297a4bb
neelmaniya <neelmaniya81@gmail.com>
2023-08-05 08:15:14 +0530

    05-08-2023

---
+++ android/app/src/main/kotlin/com/example/playtfrom_converter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.playtfrom_converter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screens/material_design/HomePage.dart
@@ -0,0 +1,59 @@
+
+import 'package:flutter/material.dart';
+import 'package:playtfrom_converter_app/providers/app_provider.dart';
+import 'package:playtfrom_converter_app/views/components/AddChat.dart';
+import 'package:playtfrom_converter_app/views/components/CallsPage.dart';
+import 'package:playtfrom_converter_app/views/components/ChatPage.dart';
+import 'package:playtfrom_converter_app/views/components/SettingsPage.dart';
+
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Platform Converter"),
+          actions: [
+            Switch(
+              value: Provider.of<AppProvider>(context).appModel.isIos,
+              onChanged: (val) {
+                Provider.of<AppProvider>(context, listen: false).switchUi();
+              },
+            ),
+          ],
+          bottom: const TabBar(
+            physics: BouncingScrollPhysics(),
+            labelStyle: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+            tabs: [
+              Icon(Icons.person_add_alt),
+              Text("CHATS"),
+              Text("CALLS"),
+              Text("SETTINGS"),
+            ],
+          ),
+        ),
+        body:  TabBarView(
+          children: [
+            AddChat(),
+            ChatPage(),
+            CallsPage(),
+            SettingsPage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
